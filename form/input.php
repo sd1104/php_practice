14bf8e5aad41fec383f3a56c5b47496d81bae81f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-29 15:26:16 +0900

    practice2

--- form/input.php
+++ form/input.php
@@ -1,34 +1,41 @@
 <?php
-  echo '<pre>';
-    var_dump($_POST);
-  echo '</pre>';
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
 
-  $pageFlag = 0;
+$pageFlag = 0;
 
-  if(!empty($_POST['btn_confirm'])) {
-    $pageFlag = 1;
-  }
-  if(!empty($_POST['btn_submit'])) {
-    $pageFlag = 2;
-  }
+if (!empty($_POST['btn_confirm'])) {
+  $pageFlag = 1;
+}
+if (!empty($_POST['btn_submit'])) {
+  $pageFlag = 2;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>input.php</title>
 </head>
+
 <body>
 
-  <?php ?>
-
-  <form method="POST" action="input.php" >
-
-  </form>
+  <?php if ($pageFlag === 0) : ?>
+    <form method="POST" action="input.php">
+      <input type="text" name="your_name">
+      <br>
+      <input type="email" name="email">
+      <br>
+      <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+  <?php endif; ?>
 
 </body>
+
 </html>
 
 <?php ?>
