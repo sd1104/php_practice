91a750b1fd9c1a95c79e72de9d3946880bfce4f1
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-29 15:30:19 +0900

    paractice3

--- form/input.php
+++ form/input.php
@@ -26,11 +26,29 @@ if (!empty($_POST['btn_submit'])) {
 
   <?php if ($pageFlag === 0) : ?>
     <form method="POST" action="input.php">
-      <input type="text" name="your_name">
+      名前
+      <input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
       <br>
-      <input type="email" name="email">
+      メールアドレス
+      <input type="email" name="email" value="<?php echo $_POST['email']; ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+  <?php endif; ?>
+
+  <?php if ($pageFlag === 1) : ?>
+    <form method="POST" action="input.php">
+      名前
+      <?php echo $_POST['your_name']; ?>
+      <br>
+      メールアドレス
+      <?php echo $_POST['email']; ?>
+      <br>
+      <input type="submit" name="back" value="戻る">
+      <input type="submit" name="btn_submit" value="送信する">
+
+      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
     </form>
   <?php endif; ?>
 
