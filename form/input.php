09985824d3d4dfae0685edc5007fdd147ea78fbf
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-29 15:43:47 +0900

    paractice7

--- form/input.php
+++ form/input.php
@@ -23,22 +23,38 @@
 <?php if($pageFlag === 0): ?>
   <form method="POST" action="input.php">
     名前
-    <input type="text" name="your_name">
+    <input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
     <br>
     メールアドレス
-    <input type="email" name="email">
+    <input type="email" name="email" value="<?php echo $_POST['email']; ?>">
     <br>
     <input type="submit" name="btn_confirm" value="確認する">
   </form>
-
 <?php endif; ?>
 
 
 <?php if($pageFlag === 1): ?>
+  <form method="POST" action="input.php">
+    名前
+    <?php echo $_POST['your_name']; ?>
+    <br>
+    メールアドレス
+    <?php echo $_POST['email']; ?>
+    <br>
+    <input type="submit" name="back" value="戻る">
+    <input type="submit" name="btn_submit" value="送信する">
+
+    <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+    <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+  </form>
 <?php endif; ?>
 
 
 <?php if($pageFlag === 2): ?>
+  送信が完了しました。
+  <form method="POST" action="input.php">
+    <input type="submit" name="top" value="トップへ">
+  </form>
 <?php endif; ?>
 
 
