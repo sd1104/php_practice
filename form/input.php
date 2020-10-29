3b21cd817f3dda29c00b741e6c27a254da1b417d
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-29 14:59:56 +0900

    view revised

--- form/input.php
+++ form/input.php
@@ -5,10 +5,10 @@ var_dump($_POST);
 echo '</pre>';
 
 $pageFlag = 0;
-if(!empty($_POST['btn_confirm'])) {
+if (!empty($_POST['btn_confirm'])) {
   $pageFlag = 1;
 }
-if(!empty($_POST['btn_submit'])) {
+if (!empty($_POST['btn_submit'])) {
   $pageFlag = 2;
 }
 
@@ -28,47 +28,48 @@ if(!empty($_POST['btn_submit'])) {
 <body>
 
 
-<?php if($pageFlag === 1): ?>
-  <form method="POST" action="input.php">
-    名前
-    <?php echo $_POST['your_name']; ?>
-    <br>
-    メールアドレス
-    <?php echo $_POST['email']; ?>
-    <br>
-    <input type="submit" name="back" value="戻る">
-    <input type="submit" name="btn_submit" value="送信する">
+  <?php if ($pageFlag === 1) : ?>
+    <form method="POST" action="input.php">
+      名前
+      <?php echo $_POST['your_name']; ?>
+      <br>
+      メールアドレス
+      <?php echo $_POST['email']; ?>
+      <br>
+      <input type="submit" name="back" value="戻る">
+      <input type="submit" name="btn_submit" value="送信する">
 
-    <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-    <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
-  </form>
-<?php endif; ?>
-
-
-
-<?php if($pageFlag === 2): ?>
-  送信が完了しました。
-  <form method="POST" action="input.php">
-    <input type="submit" name="top" value="トップへ">
-  </form>
-<?php endif; ?>
+      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+    </form>
+  <?php endif; ?>
 
 
 
-<?php if($pageFlag === 0): ?>
-  <form method="POST" action="input.php">
-    名前
-    <input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-    <br>
-    メールアドレス
-    <input type="email" name="email" value="<?php echo $_POST['email']; ?>">
-    <br>
-    <input type="submit" name="btn_confirm" value="確認する">
-  </form>
-<?php endif; ?>
+  <?php if ($pageFlag === 2) : ?>
+    送信が完了しました。
+    <form method="POST" action="input.php">
+      <input type="submit" name="top" value="トップへ">
+    </form>
+  <?php endif; ?>
+
+
+
+  <?php if ($pageFlag === 0) : ?>
+    <form method="POST" action="input.php">
+      名前
+      <input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <br>
+      メールアドレス
+      <input type="email" name="email" value="<?php echo $_POST['email']; ?>">
+      <br>
+      <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+  <?php endif; ?>
 
 
 </body>
+
 </html>
 
 <!-- ○form tag
