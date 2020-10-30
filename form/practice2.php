6522e276d315c56a013e8df27b4860ff0c28fe87
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-30 18:38:35 +0900

    Update practice2.php

--- form/practice2.php
+++ form/practice2.php
@@ -30,9 +30,37 @@ if( !empty($_POST['btn_submit']) ) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/main.css">
   <title>practice1.php</title>
 </head>
 <body>
+
+  <?php if($pageFlag == 0): ?>
+
+    <form id="form1" method="POST" action="practice.php">
+    <input type="hidden" name="csrf" value=""><?php echo h($token) ?>
+      <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+
+  <?php endif; ?>
+
+
+  <?php if($pageFlag == 1): ?>
+    <form id="form1" method="POST" action="practice.php">
+    <input type="hidden" name="csrf" value=""><?php echo h($_POST['csrf']) ?>
+      <input type="submit" name="btn_submit" value="送信する">
+    </form>
+  <?php endif; ?>
+
+
+  <?php if($pageFlag == 2): ?>
+    <form id="form1" method="POST" action="practice.php">
+      <input type="hidden" name="csrf" value=""><?php echo h($_POST['csrf']) ?>
+      <input type="submit" name="top" value="トップへ">
+    </form>
+  <?php endif; ?>
+
+
 
 </body>
 </html>
