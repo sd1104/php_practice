473b97b87c79c0621da2d8450bde4af77a790179
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-01 12:42:13 +0900

    Update practice4.php

--- form/practice4.php
+++ form/practice4.php
@@ -26,3 +26,40 @@
   }
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>pracetice4</title>
+</head>
+<body>
+
+  <section>
+    <?php if($pageFlag === 0): ?>
+      <?php
+        if( !isset($_SESSION['csrfToken']) ) {
+          $csrfToken = bin2hex(random_bytes(32));
+          $csrfToken = $_SESSION['csrfToken'];
+        }
+        $token = $_SESSION['csrfToken'];
+      ?>
+    <?php endif; ?>
+  </section>
+
+
+  <section>
+    <?php if($pageFlag === 1): ?>
+    <?php endif; ?>
+  </section>
+
+
+  <section>
+    <?php if($pageFlag === 2): ?>
+    <?php endif; ?>
+  </section>
+
+</body>
+</html>
