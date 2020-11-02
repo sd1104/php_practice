a59e34db6be7c653ef8c88bd4fb1572d29020208
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-02 14:34:59 +0900

    Update insert.php

--- maintain/insert.php
+++ maintain/insert.php
@@ -17,7 +17,18 @@
       'created_at' => null
     ];
 
+    $count = 0;
+    $columns = '';
+    $values = '';
 
+    foreach(array_keys($params) as $key) {
+      if($count++>0) {
+        $columns .= ',';
+        $values .= ',';
+      }
+      $columns .= $key;
+      $values .= ':' . $key;
+    }
 
     $sql = 'insert into `contacts` ('. $columns .')values('. $values .')';
 
