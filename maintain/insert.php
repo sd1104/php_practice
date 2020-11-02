4da8f4690b9a2cb630fdb530a3c6ccc7966feda9
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-02 13:14:24 +0900

    Update insert.php

---
+++ maintain/insert.php
@@ -0,0 +1,13 @@
+<?php
+  require 'db_connection.php';
+  $params = [
+    'id' => null,
+    'your_name' => 'なまえ',
+    'email' => 'text@text.com',
+    'url' => 'http://test.com',
+    'gender' => '1',
+    'age' => '2',
+    'contact' => 'hello, everyone.',
+    'created_at' => 'NOW()'
+  ]
+?>
