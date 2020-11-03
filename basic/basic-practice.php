d689ec1b1c4ff9975e37fd8ea4b90e2ed1123680
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-03 17:05:51 +0900

    Create basic-practice.php

---
+++ basic/basic-practice.php
@@ -0,0 +1,41 @@
+<?php
+
+// 問題A
+$array = [
+  'apple',
+  'orange',
+  'grape'
+];
+
+$array[1] = 'melon';
+$array[] = 'peach';
+
+echo '<pre>';
+  var_dump($array);
+echo '<pre>';
+
+echo $array[0];
+
+
+// 問題B
+
+
+
+// 問題C
+
+
+
+
+
+
+// 問題D
+
+
+
+
+
+
+
+
+
+?>
