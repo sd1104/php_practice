ec1c829ebfc4ec84a66c91c4e5044efac08ffb03
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-03 17:05:56 +0900

    Update basic-practice.php

--- basic/basic-practice.php
+++ basic/basic-practice.php
@@ -18,7 +18,22 @@ echo $array[0];
 
 
 // 問題B
+$boy = [
+  'name' => 'John',
+  'age' => 20,
+  'hobby' => 'fishing'
+];
+$name_keys = array_keys($boy);
 
+echo '<pre>';
+  var_dump($boy);
+echo '<pre>';
+echo '<br>';
+echo $name_keys[0] . "\n" . 'is' . '' . $boy['name'] .'' . 'and his' . '' . $name_keys[2] . '' . 'is' . '' . $boy['hobby'];
+echo '<br>';
+echo '<pre>';
+  var_dump($name_keys);
+echo '<pre>';
 
 
 // 問題C
