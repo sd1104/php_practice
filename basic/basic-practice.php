ce76639527676c8404c26f9d15a9dc4d0442c7e2
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-03 18:27:48 +0900

    Update basic-practice.php

--- basic/basic-practice.php
+++ basic/basic-practice.php
@@ -242,3 +242,31 @@ echo '<br>';
 echo '<br>';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>basic practice</title>
+</head>
+<body>
+
+  <form method="POST" action="basic-practice.php">
+    身長
+    <input type="number" name="height" value="<?php $_POST['height'] ?>">
+    <br>
+    体重
+    <input type="number" name="weight" value="<?php $_POST['weight'] ?>">
+    <br>
+    血圧
+    <input type="number" name="b_pressure" value="<?php $_POST['b_pressure'] ?>">
+    <br>
+    コレステロール値
+    <input type="number" name="colestrole" value="<?php $_POST['colestrole'] ?>">
+    <br>
+    <input type="submit" value="submit">
+  </form>
+
+</body>
+</html>
