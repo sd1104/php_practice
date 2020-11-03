8bc75e050d319c42c1665dbbdb78490d64d3d9ac
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-03 14:50:52 +0900

    Update practice5.php

--- form1/practice5.php
+++ form1/practice5.php
@@ -26,3 +26,26 @@
   echo '</pre>';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/main.js">
+  <title>form practice</title>
+</head>
+<body>
+
+  <?php if($pageFlag == 0): ?>
+  <?php endif; ?>
+  <?php if($pageFlag == 1): ?>
+  <?php endif; ?>
+  <?php if($pageFlag == 2): ?>
+  <?php endif; ?>
+
+
+
+
+</body>
+</html>
