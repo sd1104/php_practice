4f5eecd79a78fe7f3459c5361eba948f0159f849
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-04 12:23:34 +0900

    Update db_connection.php

--- practice/db/db_connection.php
+++ practice/db/db_connection.php
@@ -3,6 +3,17 @@
   const DB_USER = 'php_traing_user';
   const DB_PASSWORD = '';
 
-
+  try{
+    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_EMULATE_PREPARES => false
+    ]);
+  } catch(PDOException $e) {
+    echo 'failed';
+    echo '<br>';
+    echo $e->getMessage();
+    exit();
+  }
 
 ?>
