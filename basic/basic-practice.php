c099d39dc3eb4f21b6bd5f7ade7f1013fd6fd19c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-04 11:26:21 +0900

    Update basic-practice.php

--- basic/basic-practice.php
+++ basic/basic-practice.php
@@ -34,4 +34,73 @@ foreach($boy as $key => $value) {
   echo '<br>';
 }
 
+$height = $_POST['height'];
+
+// if ($height >= 180) {
+//   echo 'tall';
+//   echo '<br>';
+// } else if($height <= 100) {
+//  echo 'small';
+//  echo '<br>';
+// } else if ($height >100 && $height < 180) {
+//   echo 'middle';
+//   echo '<br>';
+// } else if ($height <= 0) {
+//   return;
+// }
+
+// switch($height) {
+//   case ($height >= 180):
+//     echo 'tall';
+//     echo '<br>';
+//   break;
+//   case ($height <= 100):
+//     echo 'small';
+//     echo '<br>';
+//   break;
+//   case ($height > 100 && $height < 180):
+//     echo 'tall';
+//     echo '<br>';
+//   break;
+//   default:
+//     return;
+//   break;
+// }
+
+// $high = $height >150 ? 'very tall' : 'very small';
+
+// echo $high;
+
+// echo '<pre>';
+//   var_dump($_POST);
+// echo '</pre>';
+
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>basic practice</title>
+</head>
+<body>
+
+  <form method="POST" action="basic-practice.php">
+    身長
+    <input type="number" name="height" value="<?php $_POST['height'] ?>">
+    <br>
+    <!-- 体重
+    <input type="number" name="weight" value="<?php $_POST['weight'] ?>">
+    <br>
+    血圧
+    <input type="number" name="b_pressure" value="<?php $_POST['b_pressure'] ?>">
+    <br>
+    コレステロール値
+    <input type="number" name="colestrole" value="<?php $_POST['colestrole'] ?>"> -->
+    <br>
+    <input type="submit" value="submit">
+  </form>
+
+</body>
+</html>
