65cf54ede19ea87c2869725a6be0ce58326b06ee
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-09 23:56:29 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -20,12 +20,12 @@ array_push($fruits, $tmp);
 var_dump($fruits);
 
 
-// function is_continuous_array($array)
-// {
-//     $range = range(min($array), min($array) + count($array) - 2);
-//     var_dump($range);
-//     return $range[count($range)] = ' ';
-// }
+function is_continuous_array($array)
+{
+    $range = range(min($array), min($array) + count($array) - 2);
+    var_dump($range);
+    return $range[count($range)] = ' ';
+}
 
 // var_dump(is_continuous_array([8, 6, 10, 7]));
 
