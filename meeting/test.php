58b550f437685cccfbc9263aaef5720b46f66e0d
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-10 23:15:36 +0900

    Create test.php

---
+++ meeting/test.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// function min_time($data) {
+//   if (empty($data['wish']) ) {
+//     $children['min'] = $data['min'];
+//     return $children['min'];
+//   }
+//   if (!empty($data['wish']) ) {
+//     $array_first_value = array_shift($data['wish']);
+//     return $array_first_value;
+//   }
+// }
+
+// function max_time($data) {
+//   if (empty($data['wish']) ) {
+//     $children['max'] = $data['max'];
+//     return $children['max'];
+//   }
+//   if (!empty($data['wish']) ) {
+//     $array_max_value = end($data['wish']);
+//     return $array_max_value;
+//   }
+// }
+
+?>
