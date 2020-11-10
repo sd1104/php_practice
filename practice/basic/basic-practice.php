1a553197b1e3d1cd1c79b705098f7feb8331f941
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-10 16:38:25 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -34,32 +34,3 @@ var_dump($array);
 // var_dump(is_continuous_array([8, 6, 10, 7]));
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>basic practice</title>
-</head>
-<body>
-
-  <form method="POST" action="basic-practice.php">
-    身長
-    <input type="number" name="height" value="<?php $_POST['height'] ?>">
-    <br>
-    体重
-    <input type="number" name="weight" value="<?php $_POST['weight'] ?>">
-    <br>
-    血圧
-    <input type="number" name="pressure" value="<?php $_POST['pressure'] ?>">
-    <br>
-    コレステロール値
-    <input type="number" name="colestrole" value="<?php $_POST['colestrole'] ?>">
-    <br>
-    <input type="submit" value="submit">
-  </form>
-
-</body>
-</html>
