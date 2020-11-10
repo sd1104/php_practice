ea7f129622235e1dc4d25aa9b1b06f697770cf09
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-10 23:15:26 +0900

    Create home.php

---
+++ meeting/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Makking table Kondankai</title>
+</head>
+<body>
+  <h2>Making table Kondankai</h2>
+  <h3>〜懇談会スケジュール自動作成機〜</h3>
+
+  <div>
+    <a href="new.php">スケージュール作成画面へ</a>
+  </div>
+
+  <h4>特徴</h4>
+  <p>・希望カテゴリー、もしくは希望時間を入力するだけで予定表が自動作成されます。</p>
+  <p>・自分で並べ替えたりする手間一切なし</p>
+  <p>・手紙まで一気に出力</p>
+  <p>・作業時間が短縮され、業務に専念できます。</p>
+
+</body>
+</html>
