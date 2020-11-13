be45d0a4217cdfacd84478831a5551ec03e6f2b0
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-13 17:22:46 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,20 +1,30 @@
 <?php
 
-$boys = [
-  'John' => [
-    'age' => 20,
-    'hobby' => 'fishing'
-  ],
-  'Mike' => [
-    'age' => 25,
-    'hobby' => 'dating'
-  ]
-];
+// $boys = [
+//   'John' => [
+//     'age' => 20,
+//     'hobby' => 'fishing'
+//   ],
+//   'Mike' => [
+//     'age' => 25,
+//     'hobby' => 'dating'
+//   ]
+// ];
 
-foreach($boys as $boy) {
-  print_r($boy['age']);
-  echo '<br>';
+// foreach($boys as $boy) {
+//   print_r($boy['age']);
+//   echo '<br>';
+// }
+
+$num = 0;
+// for($i=0; $i<10; $i++) {
+//   $num = ($i+1) + ($i+2);
+// }
+$i = 0;
+while($i<11) {
+  $num = ($i) + ($i+1);
+  $i++;
 }
-
+echo $num;
 
 ?>
