85a71b6335070c34c63e9c85a7ac2b13ba9fe94f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-13 17:14:27 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,39 +1,20 @@
 <?php
 
-$fruits = ['apple', 'orange', 'grape'];
-$fruits[1] = 'melon';
-$fruits[] = 'peach';
-array_push($fruits, 'grapefruit');
-array_pop($fruits);
+$boys = [
+  'John' => [
+    'age' => 20,
+    'hobby' => 'fishing'
+  ],
+  'Mike' => [
+    'age' => 25,
+    'hobby' => 'dating'
+  ]
+];
 
+foreach($boys as $boy) {
+  print_r($boy['age']);
+  echo '<br>';
+}
 
-var_dump($fruits);
-echo '<br>';
-print($fruits);
-
-// foreach($add_nums as $num) {
-//   while(true) {
-//     if(!in_array($num, $array)){
-//       array_push($array, $num);
-//       array_push($fruits, $num);
-//     break;
-//     }
-//   }
-// }
-
-
-
-// array_push($fruits, $tmp);
-// var_dump($fruits);
-
-
-// function is_continuous_array($array)
-// {
-//     $range = range(min($array), min($array) + count($array) - 2);
-//     var_dump($range);
-//     return $range[count($range)] = ' ';
-// }
-
-// var_dump(is_continuous_array([8, 6, 10, 7]));
 
 ?>
