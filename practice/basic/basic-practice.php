b777a2702e7606887873456002e4dc2d439df91a
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-17 15:16:23 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,5 +1,13 @@
 <?php
 
+$fruits = ['apple', 'oragen', 'grape'];
+$fruits[1] = 'melon';
+$fruits[] = 'peach';
+array_unshift($fruits, 'lemon');
+array_push($fruits, 'pine');
+array_shift($fruits);
+array_pop($fruits);
 
+var_dump($fruits);
 
 ?>
