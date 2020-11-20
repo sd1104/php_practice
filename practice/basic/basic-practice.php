6837b139e7f0cfbfddc45b5aab8b5d29ff71193c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-20 11:57:16 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,41 +1,46 @@
 <?php
 
-$fruits = ['apple', 'orange', 'grape'];
-// $fruits[0] = 'melon';
+// $fruits = ['apple', 'orange', 'grape'];
+// // $fruits[0] = 'melon';
 
-array_unshift($fruits, 'pineapple');
-array_shift($fruits);
-array_push($fruits, 'peach');
-array_pop($fruits);
+// array_unshift($fruits, 'pineapple');
+// array_shift($fruits);
+// array_push($fruits, 'peach');
+// array_pop($fruits);
 
 
-$boys = [
-  'john' => [
-    'age' => 20,
-    'hobby' => 'fishing'
-  ],
-  'mike' => [
-    'age' => 21,
-    'hobby' => 'singing'
-  ]
-  ];
+// $boys = [
+//   'john' => [
+//     'age' => 20,
+//     'hobby' => 'fishing'
+//   ],
+//   'mike' => [
+//     'age' => 21,
+//     'hobby' => 'singing'
+//   ]
+//   ];
 
-foreach($fruits as $f) {
-  echo $f;
-  echo '<br>';
+// foreach($fruits as $f) {
+//   echo $f;
+//   echo '<br>';
+// };
+
+// foreach($boys as $name => $info) {
+//   foreach($info as $key => $i) {
+//     echo $name;
+//     echo '<br>';
+//     echo $info;
+//     echo '<br>';
+//     echo $key;
+//     echo '<br>';
+//     echo $i;
+//     echo '<br>';
+//   }
+// }
+
+$num = 0;
+for($i=0; $i<10; $i++) {
+  $num = ($i+1) + ($i+2);
 };
-
-foreach($boys as $name => $info) {
-  foreach($info as $key => $i) {
-    echo $name;
-    echo '<br>';
-    echo $info;
-    echo '<br>';
-    echo $key;
-    echo '<br>';
-    echo $i;
-    echo '<br>';
-  }
-}
-
+echo $num;
 ?>
