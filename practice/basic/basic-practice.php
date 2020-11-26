287a93695a9605027f6d43823f69bb406275ad57
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-26 10:52:06 +0900

    Update basic-practice.php

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,46 +1,13 @@
 <?php
+$fruits = ['apple', 'orange', 'grape'];
+$fruits[] = 'melon';
+$fruits[0] = 'pine';
+array_push($fruits, 'lemon');
+array_unshift($fruits, 'melon');
+array_pop($fruits);
+array_shift($fruits);
 
-// $fruits = ['apple', 'orange', 'grape'];
-// // $fruits[0] = 'melon';
-
-// array_unshift($fruits, 'pineapple');
-// array_shift($fruits);
-// array_push($fruits, 'peach');
-// array_pop($fruits);
 
 
-// $boys = [
-//   'john' => [
-//     'age' => 20,
-//     'hobby' => 'fishing'
-//   ],
-//   'mike' => [
-//     'age' => 21,
-//     'hobby' => 'singing'
-//   ]
-//   ];
 
-// foreach($fruits as $f) {
-//   echo $f;
-//   echo '<br>';
-// };
-
-// foreach($boys as $name => $info) {
-//   foreach($info as $key => $i) {
-//     echo $name;
-//     echo '<br>';
-//     echo $info;
-//     echo '<br>';
-//     echo $key;
-//     echo '<br>';
-//     echo $i;
-//     echo '<br>';
-//   }
-// }
-
-$num = 0;
-for($i=0; $i<10; $i++) {
-  $num = ($i+1) + ($i+2);
-};
-echo $num;
 ?>
