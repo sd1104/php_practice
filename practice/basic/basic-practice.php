9344e705086a445fc96dee7569b087452d5b697f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-26 11:02:56 +0900

    initialize

--- practice/basic/basic-practice.php
+++ practice/basic/basic-practice.php
@@ -1,38 +1,4 @@
 <?php
-$fruits = ['apple', 'orange', 'grape'];
-$fruits[] = 'melon';
-$fruits[0] = 'pine';
-array_push($fruits, 'lemon');
-array_unshift($fruits, 'melon');
-array_pop($fruits);
-array_shift($fruits);
 
-$boys = [
-  'john' => [
-    'age' => 21,
-    'hobby' => 'fishing'
-  ],
-  'mike' => [
-    'age' => 22,
-    'hobby' => 'singing'
-  ]
-];
-
-// foreach($fruits as $index => $fruit) {
-//   echo $index .' ' .$fruit .' ';
-// }
-
-$lucky = rand(1,10);
-
-if($lucky === 10) {
-  echo 'lucky';
-} elseif($lucky === 5) {
-  echo 'unlucky';
-} else {
-  echo 'normal';
-}
-
-$judge = $lucky === 10? 'lucky':'unlucky';
-echo $judge;
 
 ?>
